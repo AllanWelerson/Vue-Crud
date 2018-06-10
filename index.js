98b0ddf638fa6ed312e9221b0a7d775b2b6e864f
Allanwelerson <allanwelerson@gmail.com>
2018-06-10 01:44:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+
+            pessoa1 = []; 
+              
+
+            var app = new Vue({
+                el: "#app",
+                data:{
+                    
+                    pessoas:[
+                        {id:'1',nome:'asfd',valor:'3333'},
+                        {id:'2',nome:'eeeeeee',valor:'2222'}
+                    ],
+                    pessoa: {id:'',nome:'',valor:''},
+                    pessoaAlterar: {}
+                    
+                }
+                ,
+             methods: {
+                gravar: function(){
+                        this.pessoas.push(this.pessoa);
+                        this.pessoa = Vue.util.extend({},this.pessoa);
+                        this.pessoa = {id:'',nome:'',valor:''}; 
+                },
+                editar: function(){
+                       
+                        var indice = this.pessoas.indexOf(this.pessoaAlterar);
+                        this.pessoas.splice(indice,1,this.pessoa);
+                        this.pessoa = Vue.util.extend({},this.pessoa);
+                        this.pessoa = {id:'',nome:'',valor:''};
+                   
+                },
+                apagar: function(pessoaExcluir){
+                    var indice = this.pessoas.indexOf(pessoaExcluir);
+                   
+                    this.pessoas.splice(indice,1);
+                },
+                alterar: function(pessoaAlterar){
+                        this.pessoaAlterar = pessoaAlterar;
+                        this.pessoa = Vue.util.extend({},pessoaAlterar);
+    
+                }
+            }
+    
+            })
+    
+           
+    
+    
+            
+    
